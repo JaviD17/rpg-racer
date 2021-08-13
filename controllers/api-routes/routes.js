9749ca041d741d2ce0a1dd23576587df18b4d008
Javier Dominguez II <Javi7057@gmail.com>
2021-08-12 20:34:15 -0500

    added User model

--- controllers/api-routes/routes.js
+++ controllers/api-routes/routes.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const { Op } = require('sequelize');
 
 router.get('/', (req, res) => {
-    res.render('index'); 
-    
-  });
+  res.render('index');
+});
 
 module.exports = router;
---
+++ models/User.js
@@ -0,0 +1,8 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+// require path for authenticator
+
+class User extends Model {
+    // check password with method checkPassword(loginPw) { return; };
+}
+
