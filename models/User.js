0eeed4c6aaedeecfd9f78f28187edbd34c9310e7
Javier Dominguez II <Javi7057@gmail.com>
2021-08-12 20:48:11 -0500

    finished user model with username requirement of length min of 4 and password length min of 6

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // require path for authenticator
 
@@ -6,3 +6,44 @@ class User extends Model {
     // check password with method checkPassword(loginPw) { return; };
 }
 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            }
+        }
+    },
+    {
+        // hooks {} for password hashing
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+)
+
+module.exports = User;
