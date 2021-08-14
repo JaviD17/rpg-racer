edff81e956bcf51e970ba60e0a00e9727172a895
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-08-14 12:25:32 -0500

    changed user_id in comment.js to not allow null

--- models/Comment.js
+++ models/Comment.js
@@ -24,6 +24,7 @@ Comment.init(
         },
         user_id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             references: {
                 model: 'user',
                 key: 'id'
