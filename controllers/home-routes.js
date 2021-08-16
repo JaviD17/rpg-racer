c6e2137fce3ee22d8fc81f13855b2c5b970ce230
praiseopara <cpopara2010@yahoo.com>
2021-08-15 20:00:14 -0500

    sessions added and logout route works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,12 +4,17 @@ const sequelize = require('../config/connection');
 const { User } = require('../models');
 
 router.get('/', (req, res) => {
+  //console.log(req.session);
   res.render('index');
 });
 
 //login route
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
   res.render('login');
-})
+});
 
 module.exports = router;
