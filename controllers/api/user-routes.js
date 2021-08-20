16523061986b897d27e980e3620ed200ce9ef671
Javier Dominguez II <Javi7057@gmail.com>
2021-08-19 20:10:21 -0500

    fixed login function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -40,7 +40,7 @@ router.get("/:id", (req, res) => {
 });
 
 //create user
-router.post("/", withAuth, (req, res) => {
+router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -60,6 +60,40 @@ router.post("/", withAuth, (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+//login route
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "No user found with this email address!" });
+      return;
+    }
+    async function checkPw() {
+      const verifiedPw = await dbUserData.checkPassword(req.body.password);
+      //   console.log(verifiedPw);
+      if (!verifiedPw) {
+        res.status(400).json({ message: "Incorrect password" });
+        return;
+      }
+    }
+    checkPw();
+
+    req.session.save(() => {
+      // session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
 });
 
 //update user
@@ -101,40 +135,6 @@ router.delete("/:id", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-
-//login route
-router.post("/login", (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  }).then((dbUserData) => {
-    if (!dbUserData) {
-      res
-        .status(400)
-        .json({ message: "No user found with this email address!" });
-      return;
-    }
-    async function checkPw() {
-      const verifiedPw = await dbUserData.checkPassword(req.body.password);
-      //   console.log(verifiedPw);
-      if (!verifiedPw) {
-        res.status(400).json({ message: "Incorrect password" });
-        return;
-      }
-    }
-    checkPw();
-
-    req.session.save(() => {
-      // session variables
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: "You are now logged in!" });
-    });
-  });
 });
 
 // logout route
