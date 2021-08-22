0cb85dc37e7c67a11c6f5754b3ba0c0af5039ac8
Javier Dominguez II <Javi7057@gmail.com>
2021-08-22 09:41:26 -0500

    changed const sequelize to let in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-const sequelize = process.env.JAWSDB_URL
+let sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
