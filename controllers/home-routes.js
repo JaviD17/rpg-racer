3d8f9a203d2325df25ad0607b8cd18aedbe93418
Javier Dominguez II <Javi7057@gmail.com>
2021-08-21 19:47:36 -0500

    login now works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,11 +5,9 @@ const { User } = require("../models");
 
 router.get("/", (req, res) => {
   //console.log(req.session);
-  res.render("index");
-
-  // , {
-  // loggedIn: req.session.loggedIn,
-  // }
+  res.render("index", {
+    loggedIn: req.session.loggedIn,
+  });
 });
 
 //login route
